8e442ab1463642c3ead61530f5acd02d631de52c
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-09-09 19:40:56 +0300

    implemented memory considerate client hints prototype in the basic level

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="root-frame">
+          <img src="memory-considerate-image" className="responsive" alt="spider man" />
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default App;
